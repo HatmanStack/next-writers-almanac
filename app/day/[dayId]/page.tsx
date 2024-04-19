c4b250666e3beeab08c8931d9a069b5b5456e38e
HatmanStack <hatmanstack@protonmail.com>
2024-04-18 23:52:24 -0700

    Typed

--- app/day/[dayId]/page.tsx
+++ app/day/[dayId]/page.tsx
@@ -1,30 +1,26 @@
 import Poem from '../../components/poem';
 import Note from '../../components/note';
 
+export async function generateStaticParams() {
+  return [{ params: { id: '01' } }, { params: { id: '02' } }];
+}
 
-  export async function generateStaticParams() {
-    return [{ id: '01' }, { id: '02' }]
-  }
+async function getData(params: { id: string }) {
+  const res = await fetch(
+    `https://d3vq6af2mo7fcy.cloudfront.net/public/2017/01/${params.id}.json`
+  );
+  const data = await res.json();
+  return data;
+}
 
-  async function getData(params) {
-    const res = await fetch(`https://d3vq6af2mo7fcy.cloudfront.net/public/2017/01/${params.id}.json`)
-    const data = await res.text()
-   
-    return data
-  }
+export default async function Page({ params }: { params: { id: string } }) {
+  const data = await getData(params);
 
-  export default async function Page({ params }) {
-    const data = await getData(params)
-   
-    return (
-      <div className="main-content">
-      {/* Render day-specific data here */}
+  return (
+    <div className="main-content">
       <h1>{data.date}</h1>
-      <h3>{data.dayOfWeek}</h3> 
-      {/* ...Other Day components for transcript, etc. */}
-  
-      {/* TRANSCRIPT BUTTON */}
-      <Poem 
+      <h3>{data.dayOfWeek}</h3>
+      <Poem
         poemTitle={data.poemTitle}
         poem={data.poem}
         poemByline={data.poemByline}
@@ -32,5 +28,5 @@ import Note from '../../components/note';
       />
       <Note note={data.notes} />
     </div>
-    ); 
-  }
+  );
+}
--- app/poem/[poemId]/page.tsx
+++ app/poem/[poemId]/page.tsx
@@ -1,29 +1,27 @@
-import PoemDetails from '../../components/poemdetails'; 
+import PoemDetails from '../../components/poemdetails';
 import sortedPoems from '../../../public/Poems_sorted.js';
 
-  export async function generateStaticParams() {
-    const paths = sortedPoems.map(poem => ({ id: poem }));
-    return paths;
-  }
+export async function generateStaticParams() {
+  const paths = sortedPoems.map((poem) => ({ params: { id: poem } }));
+  return paths;
+}
 
-  async function getData(params) {
-    const res = await fetch(`https://d3vq6af2mo7fcy.cloudfront.net/public/authors/${params.id}.json`)
-    const data = await res.text()
-   
-    return data
-  }
+async function getData(params: { id: string }) {
+  const res = await fetch(`https://d3vq6af2mo7fcy.cloudfront.net/public/authors/${params.id}.json`);
+  const data = await res.json();
+  return data;
+}
 
-  export default async function Page({ params }) {
-    const data = await getData(params)
-   
-    return (
-        <div className="main-content">
-            <PoemDetails 
-              poemTitle={data.poemTitle}
-              authorName={data.authorName}
-              poem={data.poem}
-              analysis={data.analysis}
-            /> 
-          </div>
-    ); 
-  }
+export default async function Page({ params }: { params: { id: string } }) {
+  const data = await getData(params);
+  return (
+    <div className="main-content">
+      <PoemDetails
+        poemTitle={data.poemTitle}
+        authorName={data.authorName}
+        poem={data.poem}
+        analysis={data.analysis}
+      />
+    </div>
+  );
+}
